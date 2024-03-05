31fedf5b0051df098259196072ca087c883c6c17
Berie <bberaki2015@gmail.com>
2024-03-04 21:46:14 -0800

    created Error.jsx, schemas and modified package.json, App.jsx, main.jsx and vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+   // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
     proxy: {
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
       }
     }
   }
