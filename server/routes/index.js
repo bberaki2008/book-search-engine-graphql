a1c193b200df659f1386f16b8a8a396329409549
Berie <bberaki2015@gmail.com>
2024-03-04 20:25:46 -0800

    modified server.js adn package.json

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-
+ 
 // serve up react front-end in production
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
